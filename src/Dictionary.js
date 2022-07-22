c04e33607deb548420adff25576d479b4941d682
ArteAvelar <arte.avelar@gmail.com>
2022-07-22 15:25:58 +0200

    added api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`searching for ${keyword}`);
-    console.log(keyword);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function changeKeyword(event) {
     setKeyword(event.target.value);
-    console.log(event.target.value);
   }
 
   return (

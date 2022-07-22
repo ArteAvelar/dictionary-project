8a612165ee49c91da3b400690fd6cacb8a2d549d
ArteAvelar <arte.avelar@gmail.com>
2022-07-22 16:45:07 +0200

    added the results and meanings

--- src/Results.js
+++ src/Results.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Meanings from "./Meanings";
 
 export default function Results(props) {
-  console.log(props.data);
-
   if (props.data) {
-    return <div className="results">Hello from results</div>;
+    return (
+      <div className="results">
+        <h2>{props.data.word}</h2>
+        {props.data.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meanings meaning={meaning} />
+            </div>
+          );
+        })}
+      </div>
+    );
   } else {
     return null;
   }
